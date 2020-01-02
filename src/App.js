8fde84562163a7481f1fec0b995f6ca468ae2a3c
maxFritzen <max.fritzen@hotmail.se>
2020-01-02 14:52:59 +0100

    feat: basic proof of concept. Works with addition 1-10 + 1-10

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max)) + 1;
+}
+
+const getExpression = () => {
+  // skicka object data med 'fråga' och 'svar'.
+  const a = getRandomInt(10)
+  const b = getRandomInt(10)
+  const answer = a + b
+  const question = `${a} + ${b}`
+  return {
+    answer,
+    question
+  }
+}
+
+const App = () => {
+  const [ question, setQuestion ] = useState('')
+  const [ inputValue, setInputValue ] = useState('')
+  const [ isAnswerCorrect, setIsAnswerCorrect ] = useState(false)
+  const [ help, setHelp ] = useState(null)
+  useEffect(() => {
+    console.log('mount');
+    setQuestion(getExpression())
+  }, [])
+
+  
+  const handleOnClick = (e) => {
+    e.preventDefault()
+    console.log('onclick', typeof inputValue, typeof question.answer)
+    if (question.answer === parseInt(inputValue)) {
+      console.log('correct')
+      setIsAnswerCorrect(true)
+      
+    } else {
+      console.log('wrong answer')
+    }
+  }
+
+  const handleInput = (e) => {
+    const value = e.target.value
+    setInputValue(value)
+  }
+
+  const newQuestion = () => {
+    setQuestion(getExpression())
+    setInputValue('')
+    setIsAnswerCorrect(false)
+    setHelp(null)
+  }
+
+  const handleHelp = () => {
+    setHelp(question.answer)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <div>
+    <div> {question.question} = {help && help}</div>
+    <form type='' onSubmit={handleOnClick}>
+    <div> svar:<input type='number' value={inputValue} onChange={(e) => handleInput(e)} /></div>
+    <div>{ isAnswerCorrect 
+      ? <button type='submit'onClick={newQuestion}>Next</button>
+      : <button type='submit'onClick={handleOnClick}>Rätta</button>
+      }</div>
+    <div><button type='button' onClick={handleHelp}>Help</button></div>
+    </form>
+  </div>
+  )
 }
 
 export default App;
